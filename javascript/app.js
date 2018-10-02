5b40eae7f5a3c24fc1e01a0aad3ad4c6b8f957d4
Andy Prudhomme <aprudhomme9@gmail.com>
2018-10-02 11:47:34 -0500

    deck rebuilds when card count gets to certain point

--- javascript/app.js
+++ javascript/app.js
@@ -18,8 +18,7 @@ const values = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 
 
 const suits = ['hearts', 'diamonds', 'spades', 'clubs'];
 
-const deck = [];
-const cardsAvailable = deck;
+let deck = [];
 const buildDeck = () => {
 	for(let i = 0; i < values.length; i++) {
 		for(let j = 0; j < suits.length; j++) {
@@ -32,63 +31,64 @@ const buildDeck = () => {
 			let card = new Card(suits[j], values[i], weight);
 			deck.push(card);
 		} 
-	} 
+	}
+	deck[0].image = "images/2-hearts.png";
+	deck[1].image = "images/2-diamonds.png";
+	deck[2].image = "images/2-spades.png";
+	deck[3].image = "images/2-clubs.png";
+	deck[4].image = "images/3-hearts.png";
+	deck[5].image = "images/3-diamonds.png";
+	deck[6].image = "images/3-spades.png";
+	deck[7].image = "images/3-clubs.png";
+	deck[8].image = "images/4-hearts.png";
+	deck[9].image = "images/4-diamonds.png"
+	deck[10].image = "images/4-spades.png";
+	deck[11].image = "images/4-clubs.png";
+	deck[12].image = "images/5-hearts.png";
+	deck[13].image = "images/5-diamonds.png"
+	deck[14].image = "images/5-spades.png";
+	deck[15].image = "images/5-clubs.png";
+	deck[16].image = "images/6-hearts.png";
+	deck[17].image = "images/6-diamonds.png"
+	deck[18].image = "images/6-spades.png";
+	deck[19].image = "images/6-clubs.png";
+	deck[20].image = "images/7-hearts.png";
+	deck[21].image = "images/7-diamonds.png"
+	deck[22].image = "images/7-spades.png";
+	deck[23].image = "images/7-clubs.png";
+	deck[24].image = "images/8-hearts.png";
+	deck[25].image = "images/8-diamonds.png"
+	deck[26].image = "images/8-spades.png";
+	deck[27].image = "images/8-clubs.png";
+	deck[28].image = "images/9-hearts.png";
+	deck[29].image = "images/9-diamonds.png"
+	deck[30].image = "images/9-spades.png";
+	deck[31].image = "images/9-clubs.png";
+	deck[32].image = "images/10-hearts.png";
+	deck[33].image = "images/10-diamonds.png"
+	deck[34].image = "images/10-spades.png";
+	deck[35].image = "images/10-clubs.png";
+	deck[36].image = "images/jack-hearts.png"
+	deck[37].image = "images/jack-diamonds.png"
+	deck[38].image = "images/jack-spades.png"
+	deck[39].image = "images/jack-clubs.png"
+	deck[40].image = "images/queen-hearts.png"
+	deck[41].image = "images/queen-diamonds.png"
+	deck[42].image = "images/queen-spades.png"
+	deck[43].image = "images/queen-clubs.png"
+	deck[44].image = "images/king-hearts.png"
+	deck[45].image = "images/king-diamonds.png"
+	deck[46].image = "images/king-spades.png"
+	deck[47].image = "images/king-clubs.png"
+	deck[48].image = "images/ace-hearts.png"
+	deck[49].image = "images/ace-diamonds.png"
+	deck[50].image = "images/ace-spades.png"
+	deck[51].image = "images/ace-clubs.png" 
 }
 
-buildDeck();
 
-deck[0].image = "images/2-hearts.png";
-deck[1].image = "images/2-diamonds.png";
-deck[2].image = "images/2-spades.png";
-deck[3].image = "images/2-clubs.png";
-deck[4].image = "images/3-hearts.png";
-deck[5].image = "images/3-diamonds.png";
-deck[6].image = "images/3-spades.png";
-deck[7].image = "images/3-clubs.png";
-deck[8].image = "images/4-hearts.png";
-deck[9].image = "images/4-diamonds.png"
-deck[10].image = "images/4-spades.png";
-deck[11].image = "images/4-clubs.png";
-deck[12].image = "images/5-hearts.png";
-deck[13].image = "images/5-diamonds.png"
-deck[14].image = "images/5-spades.png";
-deck[15].image = "images/5-clubs.png";
-deck[16].image = "images/6-hearts.png";
-deck[17].image = "images/6-diamonds.png"
-deck[18].image = "images/6-spades.png";
-deck[19].image = "images/6-clubs.png";
-deck[20].image = "images/7-hearts.png";
-deck[21].image = "images/7-diamonds.png"
-deck[22].image = "images/7-spades.png";
-deck[23].image = "images/7-clubs.png";
-deck[24].image = "images/8-hearts.png";
-deck[25].image = "images/8-diamonds.png"
-deck[26].image = "images/8-spades.png";
-deck[27].image = "images/8-clubs.png";
-deck[28].image = "images/9-hearts.png";
-deck[29].image = "images/9-diamonds.png"
-deck[30].image = "images/9-spades.png";
-deck[31].image = "images/9-clubs.png";
-deck[32].image = "images/10-hearts.png";
-deck[33].image = "images/10-diamonds.png"
-deck[34].image = "images/10-spades.png";
-deck[35].image = "images/10-clubs.png";
-deck[36].image = "images/jack-hearts.png"
-deck[37].image = "images/jack-diamonds.png"
-deck[38].image = "images/jack-spades.png"
-deck[39].image = "images/jack-clubs.png"
-deck[40].image = "images/queen-hearts.png"
-deck[41].image = "images/queen-diamonds.png"
-deck[42].image = "images/queen-spades.png"
-deck[43].image = "images/queen-clubs.png"
-deck[44].image = "images/king-hearts.png"
-deck[45].image = "images/king-diamonds.png"
-deck[46].image = "images/king-spades.png"
-deck[47].image = "images/king-clubs.png"
-deck[48].image = "images/ace-hearts.png"
-deck[49].image = "images/ace-diamonds.png"
-deck[50].image = "images/ace-spades.png"
-deck[51].image = "images/ace-clubs.png"
+
+
 // console.log(deck);
 
 /********* 
@@ -120,14 +120,14 @@ const player = {
 
 	hit () {
 		if(this.hasHit === null && this.getHandVal() < 21) {
-			this.hand.push(cardsAvailable[0]);
-			cardsAvailable.splice(0, 1);
+			this.hand.push(deck[0]);
+			deck.splice(0, 1);
 			$('#playerHand').text('Player: ' + player.getHandVal());
 			$('#hitCard1').show();
 			$('#hitCard1').attr('src', player.hand[2].image);
 		} else if(this.hasHit) {
-			this.hand.push(cardsAvailable[0]);
-			cardsAvailable.splice(0, 1);
+			this.hand.push(deck[0]);
+			deck.splice(0, 1);
 			$('#playerHand').text('Player: ' + player.getHandVal());
 			$('#hitCard2').show();
 			$('#hitCard2').attr('src', player.hand[3].image);
@@ -153,8 +153,8 @@ const dealer = {
 	// if the dealers hand is less than or equal to seventeen and is less than or equal to player hand, the dealer will hit
 	hit() {
 		while(player.stay && this.getHandVal() <= 17 && this.getHandVal() <= player.getHandVal()) {
-			this.hand.push(cardsAvailable[0]);
-			cardsAvailable.splice(0, 1);
+			this.hand.push(deck[0]);
+			deck.splice(0, 1);
 			$('#dealerHit').delay(100000).show();
 			$('#dealerHit').attr('src', dealer.hand[2].image);
 		} $('#dealerHand').text('Dealer: ' + dealer.getHandVal());
@@ -176,6 +176,7 @@ const game = {
 		$('#dealer2').attr('src', dealer.hand[1].image);
 	},
 	play() {
+		buildDeck();
 		this.shuffle(deck);
 		this.deal();
 		this.renderCards();
@@ -188,19 +189,28 @@ const game = {
 	},
 	// this will reset after a winner is decided so that we can play again
 	reset() {
+		if(deck.length < 20) {
+			deck = [];
+			this.makeNewDeck();
+		}
 		$('#deal').hide();
-		player.hasHit = null;
-		player.hand = [];
-		dealer.hand = [];
-		// this isn't working -- need to figure out how to rebuild deck
-		// if(cardsAvailable.length < 10) {
-		// 	buildDeck();
-		// 	console.log('building deck');
-		// }
+		this.clearHands();
 		this.shuffle(deck);
 		this.deal();
 		this.renderCards();
 		// this.checkWinner();
+	},
+	clearHands() {
+		player.hand=[];
+		dealer.hand=[];
+		player.hasHit = null;
+	},
+	makeNewDeck() {
+		this.clearHands();
+		buildDeck();
+		this.shuffle(deck);
+		this.deal();
+		this.renderCards();
 	},
 	playerWins() {
 		if(player.stay === true && player.getHandVal() <= 21 && player.getHandVal() > dealer.getHandVal()) {
@@ -262,10 +272,10 @@ const game = {
 	},
 	deal() {
 		for(let i = 0; i < 2; i ++) {
-			player.hand.push(cardsAvailable[0]);
-			cardsAvailable.splice(0, 1);
-			dealer.hand.push(cardsAvailable[0]);
-			cardsAvailable.splice(0,1);
+			player.hand.push(deck[0]);
+			deck.splice(0, 1);
+			dealer.hand.push(deck[0]);
+			deck.splice(0,1);
 		} 
 			$('#playerHand').text('Player: ' + player.getHandVal());
 			$('#dealerHand').text('Dealer: ' + dealer.hand[1].weight);
